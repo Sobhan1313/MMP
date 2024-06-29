bd8a1c3ca4d3a520f9493614e35f095ae6ae1be8
markuserb <markus-erb@gmx.de>
2024-06-29 19:57:15 +0200

    added Alien 3 (ZigZag Alien)

---
+++ Assets/Aliens/Scripts/Alien3Controller.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien3Controller : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+    [SerializeField]
+    public string target;    //Ziel, welches Benutzer angibt
+    [SerializeField]
+    private float zigzagFrequency; // Wie oft sich die Richtung ändert
+    [SerializeField]
+    private float zigzagAmplitude; // Wie weit die Bewegung seitlich geht
+    [SerializeField]
+    private int health;
+    private GameObject targetFound; // Zielpunkt, auf den sich der Alien zubewegt
+    private Rigidbody2D rb2d;
+    private Animator animator;
+   
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        if (target != null)
+    {
+        targetFound = GameObject.Find(target); // Beispiel: Finde das GameObject mit dem Namen "Player"
+    }
+    }
+
+    void FixedUpdate()
+    {
+        MoveTowardsTargetInZigZag();
+        RotateTowardsTarget();
+    }
+
+    void MoveTowardsTargetInZigZag()
+    {
+        if (targetFound != null)
+        {
+            // Berechne die Richtung zum Zielpunkt
+            Vector2 direction = (targetFound.transform.position - transform.position).normalized;
+
+            // Orthogonale Richtung zur Hauptbewegungsrichtung berechnen
+            Vector2 orthogonalDirection = new Vector2(-direction.y, direction.x).normalized;
+
+            // Zick-Zack-Bewegung entlang der orthogonalen Richtung hinzufügen
+            Vector2 zigzagOffset = orthogonalDirection * Mathf.Sin(Time.time * zigzagFrequency) * zigzagAmplitude;
+
+            // Kombinierte Bewegungsrichtung
+            Vector2 combinedDirection = direction + zigzagOffset;
+
+            rb2d.velocity = combinedDirection * speed;
+
+        
+        }
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+          
+        }
+    }
+
+    void RotateTowardsTarget()
+    {
+        if (targetFound != null)
+        {
+            Vector2 direction = (targetFound.transform.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Alien"))
+    {
+        // Do nothing
+        return;
+    } else {
+        health--;
+        Debug.Log("Alien collided with " + collision.gameObject.name);
+        if (health <= 0) {
+             rb2d.velocity = Vector2.zero;
+             Destroy(gameObject);    //Alien wird bei Kollision zerstört
+        }
+    }
+
+    
+    }
+}
