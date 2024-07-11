3b4f137951b08cd2f3a6443c23d67ed69ea2b512
Yusuf Yagiz Camadan <camadan@cip.ifi.lmu.de>
2024-07-11 23:25:18 +0200

    Added new alien laser (handmade) and minimap upgrade station

--- Assets/Aliens/Scripts/AlienSpawner.cs
+++ Assets/Aliens/Scripts/AlienSpawner.cs
@@ -43,6 +43,7 @@ public class AlienSpawner : MonoBehaviour
         {
             case 1:
                 selectedAliens.Add(Aliens[0]);
+                selectedAliens.Add(Aliens[3]);
                 break;
             case 2:
                 selectedAliens.Add(Aliens[0]);
